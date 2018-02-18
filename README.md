37a4afb70de79a838f7d8090d2d575c3a0ded21a
ChouaibHamek <11193161+ChouaibHamek@users.noreply.github.com>
2018-02-19 00:13:06 +0200

    merge with profile signup

---
+++ README.md
@@ -0,0 +1,2 @@
+# android-client
+Android application to assist caregivers, families, and clinicians to take care of autistic children
---
+++ app/src/main/java/com/example/chouaib/access_autism/FamilyCaregiverSignUpForm3Activity.kt
@@ -0,0 +1,12 @@
+package com.example.chouaib.access_autism
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FamilyCaregiverSignUpForm3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_family_caregiver_sign_up_form3)
+    }
+}
