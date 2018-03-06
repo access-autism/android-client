9a8dab285d3b1ba23c51c79e380b886297be6c76
ChouaibHamek <11193161+ChouaibHamek@users.noreply.github.com>
2018-03-06 16:30:32 +0200

    Add Skill Layout Detail

---
+++ app/src/main/java/com/example/chouaib/access_autism/SkillDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chouaib.access_autism
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill_detail)
+    }
+}
